6817c2c5d726a4742dda7c3515e11946aa377908
Iuri Gabriel <iurignogueira@gmail.com>
2023-07-11 00:40:48 -0300

    update-dto

--- src/main/java/br/com/iuri/restaurantadvisorapi/dto/RestaurantDto.java
+++ src/main/java/br/com/iuri/restaurantadvisorapi/dto/RestaurantDto.java
@@ -12,20 +12,35 @@ public class RestaurantDto {
 
     private String name;
     private String type;
+    private String address;
+    private String details;
     private String photos;
+    private String hours;
+
     private String rating;
     private String cost;
 
     public RestaurantDto(Restaurant restaurant){
-        this.photos = restaurant.getPhotos();
-        this.type = restaurant.getType();
         this.name = restaurant.getName();
+        this.type = restaurant.getType();
+        this.address = restaurant.getAddress();
+        this.details = restaurant.getDetails();
+        this.photos = restaurant.getPhotos();
+        this.hours = restaurant.getHours();
         this.rating = restaurant.getRating();
         this.cost = restaurant.getCost();
     }
 
     public static RestaurantDto fromRestaurant(Restaurant restaurant){
-        return new RestaurantDto(restaurant.getPhotos(), restaurant.getType(), restaurant.getName(), restaurant.getRating(), restaurant.getCost());
+        return new RestaurantDto(
+                restaurant.getName(),
+                restaurant.getType(),
+                restaurant.getAddress(),
+                restaurant.getDetails(),
+                restaurant.getPhotos(),
+                restaurant.getHours(),
+                restaurant.getRating(),
+                restaurant.getCost());
     }
 
 }
